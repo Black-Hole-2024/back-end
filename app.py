eb98f5306b0039d5798ae4f3d8cc2ffcda89f2f6
Masri <masrialemu404@gmail.com>
2024-07-21 00:32:37 +0300

    v0.3

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ from user.user_register import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#postgresql://sign_backend_user:zzlw2WpNJYBhoDpzkWtCdJFRTQcptzqL@dpg-cqe2hv0gph6c73agsk6g-a.oregon-postgres.render.com/sign_backend
+#PGPASSWORD=zzlw2WpNJYBhoDpzkWtCdJFRTQcptzqL psql -h dpg-cqe2hv0gph6c73agsk6g-a.oregon-postgres.render.com -U sign_backend_user sign_backend
